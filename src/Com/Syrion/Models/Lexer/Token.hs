5750cb730c0e4cef6afbf8480c402d3752ad3164
GeovaniMtz <geovanimartinez14@ciencias.unam.mx>
2025-10-06 10:01:11 -0600

    Agrega definición de TokenKind

--- src/Com/Syrion/Models/Lexer/Token.hs
+++ src/Com/Syrion/Models/Lexer/Token.hs
@@ -1,2 +1,14 @@
--- src/Com/Syrion/Models/Lexer/Token.hs
-module Com.Syrion.Models.Lexer.Token where
+module Com.Syrion.Models.Lexer.Token
+  ( TokenKind(..) ) where
+
+data TokenKind
+  -- Palabras reservadas
+  = KIf | KThen | KElse | KWhile | KDo | KSkip
+  | KTrue | KFalse | KNot | KAnd
+  -- Identificadores y literales
+  | Id | IntLit
+  -- Operadores / delimitadores
+  | Plus | Minus | Times | Assign | Eq | Leq | Semi | LPar | RPar
+  -- Ignorables
+  | WS | Comment
+  deriving (Eq, Ord, Show)
